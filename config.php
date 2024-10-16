3f43c0cc2d729f9db12729b9ee595751b9309212
qingfangcloudmusic <qingfangcraft@outlook.com>
2024-10-16 19:43:55 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// +------------------------------------------------------------------------+
+// | DeepSound - The Ultimate PHP Sound Sharing Platform
+// | Copyright (c) 2019 DeepSound. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "yin";
+// MySQL Database Password
+$sql_db_pass = "tsStsiiefFNYZ8Ca";
+// MySQL Database Name
+$sql_db_name = "yin";
+
+// Site URL
+$site_url = "https://music.qingfangcomputer.top"; // e.g (http://example.com)
+
+$developer_mode = 0;
+
+// Purchase code
+$purchase_code = "庆方云音乐"; // Your purchase code, don't give it to anyone.
+?>
