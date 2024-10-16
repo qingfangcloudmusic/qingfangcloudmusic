1484252327274d25ea6396fb6cc507a5db0fbde8
qingfangcloudmusic <qingfangcraft@outlook.com>
2024-10-16 20:01:00 +0800

    Delete sources directory

--- sources/genres.php
+++
@@ -1,5 +0,0 @@
-<?php 
-$music->site_title = lang('Genres');
-$music->site_description = 'Description';
-$music->site_pagename = "genres";
-$music->site_content = loadPage("genres/content");
--- sources/upgraded.php
+++
@@ -1,9 +0,0 @@
-<?php 
-if (IS_LOGGED == false) {
-	header("Location: $site_url");
-	exit();
-}
-$music->site_title = lang('You are a pro!');
-$music->site_description = 'Description';
-$music->site_pagename = "upgraded";
-$music->site_content = loadPage("go-pro/upgraded");
